96a8777fbe305ac5df2a8f2f7b3f807538545f4b
ramonrocha1989 <ramonrocha1989@gmail.com>
2021-08-24 22:30:10 -0300

    inserting status return

--- src/index.js
+++ src/index.js
@@ -12,11 +12,23 @@ let clients = [
 
 ]
 
+function log (request, response, next){
+    const {url, method} = request;
+    console.log(`${method} - ${url} at ${new Date()}`)
+    return next();
+}
+app.use(log)
+
 app.get('/clients', (request, response) => response.json(clients))
 
 app.get('/clients/:id', (request, response) => {
-    const client = clients.filter(value =>value.id == request.params.id)
-    response.json(client)
+    const {id} = request.params;
+    const client = clients.find(value => value.id == id);
+    if(client == undefined){
+        response.status(400).json({error: 'Requisição inválida'});
+    }else{
+        response.status(200).json(client);
+    }
 })
 
 app.post('/clients', (request, response) => {
@@ -30,14 +42,23 @@ app.put('/clients/:id', (request, response) => {
     const nome = request.body.nome
 
     let client = clients.filter(value =>value.id == id) 
-    client[0].nome = nome
-    response.json(client[0])
+    if(client == undefined){
+        response.status(400).send();
+    }else{
+        client.nome = nome;
+        response.status(200).json(client);
+    }
 })
 
 app.delete('/clients/:id', (request, response) => {
-    const id = request.params.id
-    const client = clients.filter(value =>value.id != id)
-    response.json(client)
+    const {id} = request.params;
+    const index = clients.findIndex(value => value.id == id);
+    if(index == -1){
+        response.status(400).send();
+    }else{
+        clients.splice(index,1);
+        response.status(204).send();
+    }
 })
 
 app.listen(3000);
